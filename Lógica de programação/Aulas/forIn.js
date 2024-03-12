b4bfb1b188437eb3818aa9503b9c0caab2950cb0
gugonunes <gugonunes@hotmail.com>
2024-03-11 22:44:43 -0300

    add for in example

---
+++ Lógica de programação/Aulas/forIn.js
@@ -0,0 +1,15 @@
+const frutas = ['Pera', 'Maça', 'Uva'];
+
+for (let indice in frutas) {
+  console.log(frutas[indice])
+}
+
+const pessoa = {
+  nome: 'Gustavo',
+  sobrenome: 'Alexandre',
+  idade: 30
+}
+
+for (let chave in pessoa) {
+  console.log(pessoa[chave])
+}
