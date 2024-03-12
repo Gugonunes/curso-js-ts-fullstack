7582e4602cf6b3233d8bba42e64a239c7cbab9c0
gugonunes <gugonunes@hotmail.com>
2024-03-11 22:57:48 -0300

    add for of

---
+++ Lógica de programação/Aulas/forOf.js
@@ -0,0 +1,16 @@
+const nome = 'Gustavo Alexandre';
+
+for (let valor of nome) {
+  console.log(valor)
+}
+
+const nomes = [
+  'Gustavo',
+  'Alexandre',
+  'Tuchlinowicz',
+  'Nunes'
+]
+
+for (let nome of nomes) {
+  console.log(nome)
+}
